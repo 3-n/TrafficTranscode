9be98a907be0da99a172fb497144a61c1b4c9dad
Jarosław M. Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2012-09-04 22:59:07 +0200

    Should have channels in Intersection, even if it's hard

--- TrafficTranscode/RealNet/Intersection.cs
+++ TrafficTranscode/RealNet/Intersection.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
+using TrafficTranscode.MetaNet;
 
 namespace TrafficTranscode.RealNet
 {
     public class Intersection
     {
         public string Id { get; set; }
+        public IEnumerable<Channel> Channels { get; set; }
     }
 
 }
