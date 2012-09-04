9caaa9b591445f1f94e6ec6a64e3df997c8679db
Jarosław M. Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2012-09-04 22:57:45 +0200

    Console interface stub

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficTranscode.Parse;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testPath = @"E:\Politechnika\TWO\MGR\Natężenia\Głogowska - Rynek Łazarski\Poznań, Głogowska-Rynek [248] 12-06-2012.rej";
+
+            var loader = new Loader(testPath);
+
+            var records = loader.Parse();
+
+            Console.WriteLine(records.Count());
+            Console.ReadKey();
+
+        }
+    }
+}
