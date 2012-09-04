d84d18831226325afed295f5ba0958d85a725fc0
Jarosław M. Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2012-09-05 00:32:18 +0200

    Channel equals etc.

--- TrafficTranscode/MetaNet/Channel.cs
+++ TrafficTranscode/MetaNet/Channel.cs
@@ -10,5 +10,33 @@ namespace TrafficTranscode.MetaNet
         {
             return UId;
         }
+
+        protected bool Equals(Channel other)
+        {
+            return string.Equals(UId, other.UId);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Channel) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (UId != null ? UId.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Channel left, Channel right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Channel left, Channel right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
